8c8d932120d34a323b5966decd26f6f79ceeb69d
Nicholas Bishop <nicholasbishop@gmail.com>
2020-08-15 15:57:38 -0400

    Start adding args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use argh::FromArgs;
+use std::path::PathBuf;
+
+/// Tarball utility.
+#[derive(Debug, FromArgs)]
+struct Opt {
+    #[argh(subcommand)]
+    command: Command,
+}
+
+#[derive(Debug, FromArgs)]
+#[argh(subcommand)]
+enum Command {
+    List(ListCommand),
+    // TODO
+    // Pack,
+    // Unpack,
+}
+
+/// List the contents of a tarball.
+#[derive(Debug, FromArgs)]
+#[argh(subcommand, name = "list")]
+struct ListCommand {
+    #[argh(positional)]
+    tarball: PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let opt: Opt = argh::from_env();
+    dbg!(&opt);
+
+    match opt.command {
+        Command::List(list) => {}
+    }
 }
